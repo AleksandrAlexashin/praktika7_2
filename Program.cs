f3d59797b2c3533895f1377cea35404db967cada
AleksandrAlexashin <sashokmetall@mail.ru>
2021-12-11 00:19:11 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktika7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длину ребра куба-");
+            int x = Convert.ToInt32(Console.ReadLine());
+            CalcKub(x, out double v, out double s);
+            Console.WriteLine("Объём куба равен  {0} ",v);
+            Console.WriteLine("Площадь поверхности куба равна {0} ",s);
+            Console.ReadKey();
+
+        }
+        static void CalcKub (int x, out double v, out double s)
+        {
+            v = Math.Pow(x, 3);
+            s = 6 * Math.Pow(x, 2);
+            
+        }
+
+
+    }
+}
